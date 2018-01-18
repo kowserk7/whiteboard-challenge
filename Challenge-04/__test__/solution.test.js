5b58905c7aee2ec5957b3f306aee55b444fab201
Koko Kassa <kowserk7@gmail.com>
2018-01-18 14:45:39 -0800

    added the second test case and passed

--- Challenge-04/__test__/solution.test.js
+++ Challenge-04/__test__/solution.test.js
@@ -4,8 +4,11 @@ const solution = require('../lib/solution');
 
 describe ('Solution Module', () => {
   describe ('#intersect', () => {
-    it('should ..', () => {
+    it('should return an array with the matching value', () => {
       expect (solution.intersect(['a','b','c'],['c','d','e'])).toEqual(['c']);
+    });
+    it ('should contain a value in the paramaters', () => {
+      expect (solution.intersect()).toBeNull();
     });
   });
 });
--- Challenge-04/lib/solution.js
+++ Challenge-04/lib/solution.js
@@ -3,6 +3,9 @@
 const solution = module.exports = {};
 
 solution.intersect = (arrOne, arrTwo) => {
+  if (!arrOne || !arrTwo) {
+    return null;
+  }
   let newArr = [];
   arrOne.map(val => {
     for (let i = 0; i < arrTwo.length; i++) {
