cce137015532fb71fbbfcc96614ce12888b3a1e0
Koko Kassa <kowserk7@gmail.com>
2018-01-24 20:26:50 -0800

    set up my class, working on setting up tests

--- Challenge-08/lib/sll.js
+++ Challenge-08/lib/sll.js
@@ -5,20 +5,25 @@ const ND = require('../lib/nd');
 class SLL {
   constructor () {
     this.head = null;
+    this.end = null;
   }
   insertHead(val) {
-    let nd = new ND(val);
-    if(!this.head){
-      this.head = ND;
+    let nd = new ND(val, this.head);
+    this.head = nd;
+    if(!this.end){
+      this.end = nd;
       return this;
     }
   }
   insertEnd(val) {
     let nd = new ND(val);
+    if(this.tail) {
+      this.tail.next = nd;
+    }
     if(!this.head) {
       this.head = nd;
       return this;
     }
   }
 }
-module.exports = {};
+module.exports = SLL;
