d35460eb8d28f76cd7b714bf98c5f9904684cea7
Koko Kassa <kowserk7@gmail.com>
2018-01-26 18:59:44 -0800

    added both solutions

--- Challenge-10/__test__/solution-two.test.js
+++ Challenge-10/__test__/solution-two.test.js
@@ -1,11 +1,21 @@
 'use strict';
 
-const solution = require('../lib/solution-two');
+const solution = require('../lib2/solution-two');
 
 describe('Solution Module', () => {
-  describe('solution', () => {
-    it('should..', () => {
-      expect().toBe();
+  
+  describe('binary Search', () => {
+    let arr = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 44, 55];
+    let array = [];
+
+    it('should return null if input is not in given array', () => {
+      expect(solution.binarySearch(33, arr)).toBeNull();
+    });
+    it('should return the the index of n in the array', () => {
+      expect(solution.binarySearch(12, arr).index).toBe(11);
+    });
+    it('should return null if given array is empty', () => {
+      expect(solution.binarySearch(4, array)).toBeNull();
     });
   });
 });
