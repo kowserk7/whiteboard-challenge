b66f8f39c8819af6e568311d1a08d995b7bf5346
Koko Kassa <kowserk7@gmail.com>
2018-01-27 17:42:10 -0800

    fixed my test file

--- Challenge-03/lib/solution.js
+++ Challenge-03/lib/solution.js
@@ -3,8 +3,10 @@
 const solution = module.exports = {};
 
 solution.traverse = function (train) {
+  if (!train) return null;
   let total = 0;
   while (train.next !== null) {
+    if (typeof train.value !== 'number') return 'object value not a number';
     console.log(train.value);
     total += train.value;
     train = train.next;
