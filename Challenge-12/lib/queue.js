130606926a132e7484c4c26a34f3fef2dac0b984
Koko Kassa <kowserk7@gmail.com>
2018-01-30 13:21:41 -0800

    scaffold

---
+++ Challenge-12/lib/queue.js
@@ -0,0 +1,29 @@
+const Node = require('./node');
+
+module.exports =  class Queue {
+
+  constructor(maxSize= 1048) {
+    this.maxSize = maxSize;
+    this.size = 0;
+    this.top = null;
+    this.bottom = null;
+  } 
+
+  enqueue(value) {
+    if (this.size === this.maxSize) throw new Error ('Queue overflow!');
+    let node = new Node(value);
+    node.next = this.top;
+    this.top = node;
+    if (!this.end) this.bottom = node;
+    this.size++;
+    return this;
+  }
+
+  dequeue() {
+    let newTop = this.top;
+    this.top = this.top.next;
+    newTop.next = null;
+    this.size --;
+    return newTop;
+  }
+};
