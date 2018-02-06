36f9f98e2bcbdb1d2384004764e75d6871eb00c4
Koko Kassa <kowserk7@gmail.com>
2018-02-06 15:53:38 -0800

    added one more expect

--- Challenge-17/__test__/solution.test.js
+++ Challenge-17/__test__/solution.test.js
@@ -8,6 +8,11 @@ describe('Solution Module', () => {
   beforeAll (() => {
     this.tree = new k_ary().insert(1,1).insert(8,1).insert(16,1).insert(33,1);
     this.emptyNode = new k_ary();
+    console.log(this.tree);
+    // console.log(this.tree2);
+  });
+  beforeAll(() => {
+    this.tree2 = new k_ary().insert(4,4).insert(3,4).insert(2,4).insert(1,4);
   });
 
   describe('Valid input/output with three childless nodes', () => {
@@ -16,6 +21,7 @@ describe('Solution Module', () => {
     });
     it('should return an a number with the sum of the values', () => {
       expect(solution.sumValue(this.tree)).toBe(58);
+      expect(solution.sumValue(this.tree2)).toBe(10);
     });
     
   });
