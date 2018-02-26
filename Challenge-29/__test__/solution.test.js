4d4fe4940bd274f2dfc95b76f5a5696bb8d167f0
Koko Kassa <kowserk7@gmail.com>
2018-02-25 20:57:04 -0800

    finsihed code

--- Challenge-29/__test__/solution.test.js
+++ Challenge-29/__test__/solution.test.js
@@ -2,10 +2,19 @@
 
 const solution = require('../lib/solution');
 
-describe('Solution Module', () => {
-  describe('solution', () => {
-    it('should..', () => {
-      expect().toBe();
+describe('Solution Module', function() {
+  let anagram1 = ['acre', 'act', 'race', 'cat', 'tac', 'care'];
+  let anagram2 = ['acre', 'race', 'care', 'act', 'cat', 'tac'];
+
+  describe('#sortAnagrams', function() {
+    it('should return foo if no arguments passed', function() {
+      expect(solution.sortAnagrams()).toEqual('Invalid');
+    });
+    it('should return foo if empty array', function() {
+      expect(solution.sortAnagrams([])).toEqual('Invalid');
+    });
+    it('should remove any element that is not a string', function() {
+      expect(solution.sortAnagrams(anagram1)).toEqual(anagram2);
     });
   });
 });
--- Challenge-29/lib/solution.js
+++ Challenge-29/lib/solution.js
@@ -1,3 +1,17 @@
 'use strict';
 
 const solution = module.exports = {};
+solution.sortAnagrams = function(array) {
+  if (!array || array.length === 0) return 'Invalid';
+  const anagrams = {};
+  array.map(ele => {
+    if (typeof ele !== 'string') return;
+    var sortedWord = ele.split('').sort().join('');
+    anagrams[sortedWord] ? anagrams[sortedWord].push(ele) : anagrams[sortedWord] = [ele];
+  });
+  let sortedArray = [];
+  for (let property in anagrams) {
+    anagrams[property].map(ele => sortedArray.push(ele));
+  }
+  return sortedArray;
+};
