5a2839c44a659392e83cc9df5a5dfc58966fbe73
Koko Kassa <kowserk7@gmail.com>
2018-02-25 20:46:24 -0800

    completed whiteboard

--- Challenge-28/__test__/solution.test.js
+++ Challenge-28/__test__/solution.test.js
@@ -3,9 +3,15 @@
 const solution = require('../lib/solution');
 
 describe('Solution Module', () => {
-  describe('solution', () => {
-    it('should..', () => {
-      expect().toBe();
+  describe('highestScore', () => {
+    it('should return null if no arguments passed', function() {
+      expect(solution.highWord()).toEqual(null);
+    });
+    it('should return null if not a string', function() {
+      expect(solution.highWord([1, 2, 3])).toEqual(null);
+    });
+    it('should return highest scoring word', function() {
+      expect(solution.highWord('one xyxyxyxyxyxyxyxyxyxyxyxyxyxyx two eight foo xxxxxyyy abc bsdhuh whiuw hohwoghuewhgouerhgouh eroughoe')).toEqual('xyxyxyxyxyxyxyxyxyxyxyxyxyxyx');
     });
   });
 });
--- Challenge-28/lib/solution.js
+++ Challenge-28/lib/solution.js
@@ -1,3 +1,20 @@
 'use strict';
 
 const solution = module.exports = {};
+solution.highestScore = function(string){
+  var words = string.split(' '),
+    max = 0,
+    highest = '';
+  for(let i = 0; i < words.length; i++){
+    var s = words[i],
+      value = 0;
+    for(let j = 0; j < s.length; j++){
+      value += (s.charCodeAt(j) - 96);
+    }
+    if(value > max){
+      max = value;
+      highest = s;
+    }
+  }
+  return highest;
+};
